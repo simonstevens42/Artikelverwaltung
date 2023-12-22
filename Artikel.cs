6482515dd8ea202f802ac966ad39bd1c8cb08d66
simonstevens42 <mail.simon.stevens@proton.me>
2023-12-22 19:55:12 +0100

    Übung
    Übungsaufgabe um C# zu vertiefen und beizubehalten.

---
+++ Artikel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artikelverwaltung
+{
+    internal class Artikel
+    {
+        // Attribute
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        // Konstruktor
+        public Artikel(string name, double price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        // toString Methode überseteuern
+        public override string ToString()
+        {
+            return $"Name: {Name} Price: {Price}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Artikelverwaltung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tuple Liste mit Daten
+            List<(string, double)> artikelTuple = new List<(string, double)>
+            {
+                ("Produkt 1", 9.99),
+                ("Produkt 2", 8.88),
+                ("Produkt 3", 7.77),
+                ("Produkt 4", 6.66),
+                ("Produkt 5", 5.55)
+            };
+
+            // Artikel Liste
+            List<Artikel> artikel = new List<Artikel>();
+
+            // Iteration durch ArtikelT in Artikel
+            foreach ((string, double) product in artikelTuple)
+            {
+                artikel.Add(new Artikel(product.Item1, product.Item2));
+            }
+
+            // Iteration durch Artikel für Ausgabe
+            foreach(Artikel product in artikel)
+            {
+                Console.WriteLine(product.ToString());
+            }
+        }
+    }
+}
